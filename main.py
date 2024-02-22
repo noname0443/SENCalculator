6b4058d51795b17e97cce072eb85cdaec99f0949
noname0443 <noname342341@gmail.com>
2024-02-22 11:02:21 +0300

    Add IsPrime function and tests

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import prime
+
+def main():
+    value = input("Print your number: ")
+    print(prime.IsPrime(int(value)))
 
 if __name__ == "__main__":
     main()
-
-def main():
-    print("Hello world")
-
